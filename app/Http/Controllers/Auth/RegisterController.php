db5a9faac927f22d9485cd4d2ea5f93576402e64
Darren <chuhancheng@gmail.com>
2017-01-18 23:38:27 +0800

    social login bugfix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -98,7 +98,7 @@ class RegisterController extends Controller
 
         $user = User::where('facebook_id', $socialUser->getId())->first();
         if(!$user){
-            User::create([
+            $user = User::create([
                 'facebook_id' => $socialUser->getId(),
                 'name' => $socialUser->getName(),
                 'email' => $socialUser->getEmail(),
