aee411f500bdf6605bf415ff468ab35920e2aa1f
Darren <chuhancheng@gmail.com>
2017-06-09 17:59:02 +0800

    bootstrap font issue

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,6 @@ module.exports = {
             {test: /\.scss$/, loader: ExtractTextPlugin.extract('css-loader!sass-loader')},
 
             { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
-            
 
             {
 
