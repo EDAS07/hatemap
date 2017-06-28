f281e8d65af9deeba0f508028dd6f60b8533147c
Darren <chuhancheng@gmail.com>
2017-06-28 15:43:35 +0800

    logout bug fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,23 @@
     @yield('content')
 
     <!-- Scripts -->
+    
+    <script type="text/javascript">
+        if (window.location.hash == '#_=_'){
+            // Check if the browser supports history.replaceState.
+            if (history.replaceState) {
+                // Keep the exact URL up to the hash.
+                var cleanHref = window.location.href.split('#')[0];
+                // Replace the URL in the address bar without messing with the back button.
+                history.replaceState(null, null, cleanHref);
+            } else {
+                // Well, you're on an old browser, we can get rid of the _=_ but not the #.
+                window.location.hash = '';
+
+            }
+        }
+    </script>
+
     @yield('js')
 
 </body>
