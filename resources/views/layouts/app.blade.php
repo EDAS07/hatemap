971656e4182718c6e49f8bc466eb396176643506
Darren <chuhancheng@gmail.com>
2017-06-29 10:35:42 +0800

    disable mobile zoom in action when user typing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Disable zoom in action when typing -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
